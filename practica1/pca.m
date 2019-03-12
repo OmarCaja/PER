e5d2ac4b3025acb72b1a3b8f23daf9b9abececc7
CULLA DE MOYA <joculde@EVIRL-011-OK.dsic.cloud>
2019-03-12 18:56:56 +0100

    completed pca implementation

--- practica1/pca.m
+++ practica1/pca.m
@@ -1,3 +1,8 @@
 function [m,W]=pca(X)
-  %% TODO
+  m = mean(X')'; % Calculamos la media
+  A = X - m;     % Restamos la media al vector dato
+  Cov = 1/columns(X)*A*A'; %Obtenemos la matriz de covarianzas
+  [eigV, eigVal] = eig(Cov); %Obtenemos autovalores y autovectores de Cov
+  [_, I] = sort(diag(eigVal), "descend"); %Obtenemos el vector de indices de sort
+  W = eigV(:,I); %obtenemos los autovectores ordendos segun su autovalor desec.
 endfunction
