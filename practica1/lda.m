dd32982bf1b6a95df3add16afdba93123a54ebd6
CAJA GARCÍA <omcagar@EVIRL-016-OK.dsic.cloud>
2019-04-02 18:57:11 +0200

    Started LDA

--- practica1/lda.m
+++ practica1/lda.m
@@ -1,3 +1,8 @@
-function [W]=lda(X,x1)
-  %% TODO
-endfunction
+load trdata.mat.gz
+load trlabels.mat.gz
+m = mean(X')'; % Calculamos la media
+
+for c = unique(xl)
+  ic = find (xl == c);
+  xc = X(:, ic);
+
