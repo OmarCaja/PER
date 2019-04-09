e21be7facf5f2c40bcaa788c299572922a7ec072
CAJA GARCÍA <omcagar@EVIRL-017-OK.dsic.cloud>
2019-04-09 18:49:57 +0200

    Finish LDA

--- practica1/lda.m
+++ practica1/lda.m
@@ -1,8 +1,32 @@
 load trdata.mat.gz
 load trlabels.mat.gz
-m = mean(X')'; % Calculamos la media
+
+m = mean(X')'; % Calculamos la media global
+
+Sb = 0;
+Sw = 0;
+Cov = 0;
 
 for c = unique(xl)
+  
   ic = find (xl == c);
   xc = X(:, ic);
+  
+  xcm = mean(xc')'; % Calculamos la media de cada clase
+  
+  Sb = Sb + columns(ic)*(xcm - m)*(xcm - m)';
+  
+  A = xc - xcm;     % Restamos la media al vector dato
+  Cov = 1/columns(A)*A*A'; %Obtenemos la matriz de covarianzas
+  
+  Sw = Sw + Cov; % Obtenemos Sw
+  
+end
 
+[eigV, eigVal] = eig(Sb,Sw); %Obtenemos autovalores y autovectores de Sb y Sw
+[_, I] = sort(diag(eigVal), "descend"); %Obtenemos el vector de indices de sort
+ W = eigV(:,I); %obtenemos los autovectores ordendos segun su autovalor desec.
+ 
+ w = W(:,5);
+ wr = reshape(w,16,16);
+ imshow(wr', []);
