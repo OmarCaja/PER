43c3cc37ebfbfd888722c5126ca4bb5145b0ad5c
Jose Culla <joseculla@outlook.com>
2019-04-14 14:08:33 +0200

    formated knn file

--- practica1/knn.m
+++ practica1/knn.m
@@ -1,25 +1,23 @@
 function [err]=knn(X,xl,Y,yl,KNN)
 
-[d,n]=size(X);
-[dt,nt]=size(Y);
+    [d,n]=size(X);
+    [dt,nt]=size(Y);
 
 
-D = pdist2( Y', X', "euclidean");
-[D,idx] = sort(D, 2, 'ascend');
+    D = pdist2( Y', X', "euclidean");
+    [D,idx] = sort(D, 2, 'ascend');
 
 
-%# KNN nearest neighbors
-D = D(:,1:KNN);
-idx = idx(:,1:KNN);
+    %# KNN nearest neighbors
+    D = D(:,1:KNN);
+    idx = idx(:,1:KNN);
 
-%# majority vote
-prediction = mode(xl'(idx),2);
+    %# majority vote
+    prediction = mode(xl'(idx),2);
 
-cerr = (yl'-prediction);
+    cerr = (yl'-prediction);
 
-err=nt-sum(cerr(:)==0);
-  
-err=(err*100.0)/nt;
+    err=nt-sum(cerr(:)==0);
 
-  
-endfunction
+    err=(err*100.0)/nt;
+end
