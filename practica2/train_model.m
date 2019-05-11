b00178bdfc03070d86199e42452918899499c800
Jose Culla <joseculla@outlook.com>
2019-05-11 17:30:04 +0200

    calculated a priori probabilities

--- practica2/train_model.m
+++ practica2/train_model.m
@@ -12,3 +12,12 @@ load(datafile);
 disp("Data load complete");
 
 [te, tr] = shuffle(data, partition);
+
+spamIndex = find(full(tr(:,end)));
+spam = tr(spamIndex, 1:(end - 1));
+
+hamIndex = find(~full(tr(:,end)));
+ham = tr(hamIndex, 1:(end - 1));
+
+spamAPriori = size(spam(:,1))(1,1) / size(tr(:,1))(1,1);
+hamAPriori = size(ham(:,1))(1,1) / size(tr(:,1))(1,1);
