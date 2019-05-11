85672ac260fa0cd20e792d9ce9aa4964e5098927
Jose Culla <joseculla@outlook.com>
2019-05-11 19:38:38 +0200

    fixed matrix clasifier

--- practica2/clasifier_matrix.m
+++ practica2/clasifier_matrix.m
@@ -1,2 +1,2 @@
-function err = clasifier_matrix(ps0, ps, ph0, ph, te)  gh = log(ph)*te(:,1:(end -1))' + log(ph0);  gs = log(ps)*te(:,1:(end -1))' + log(ps0);    aux = [gh;gs];  [_, maxIndex] = max(aux);  maxIndex--;    errors = sum(te(:,end) == maxIndex');    err = errors / rows(te);
+function err = clasifier_matrix(ps0, ps, ph0, ph, te)  gh = log(ph)*te(:,1:(end -1))' + log(ph0);  gs = log(ps)*te(:,1:(end -1))' + log(ps0);    aux = [gh;gs];  [_, maxIndex] = max(aux);  maxIndex--;  errors = sum(te(:,end) != maxIndex');    disp(errors);  err = errors / rows(te);
 endfunction
--- practica2/train_model.m
+++ practica2/train_model.m
@@ -5,8 +5,8 @@ function [ps0, ps, ph0, ph] = train_model(tr)
   hamIndex = find(~full(tr(:,end)));
   ham = tr(hamIndex, 1:(end - 1));
 
-  ps0 = size(spam(:,1))(1,1) / size(tr(:,1))(1,1);
-  ph0 = size(ham(:,1))(1,1) / size(tr(:,1))(1,1);
+  ps0 = rows(spam) / rows(tr);
+  ph0 = rows(ham) / rows(tr);
 
   ph = sum(ham)/sum(sum(ham'));
   ps = sum(spam)/sum(sum(spam'));
