b7ab65c40b25e5b9a0b06dc23149c2ce4649dc6a
Jose Culla <joseculla@outlook.com>
2019-05-16 17:55:30 +0200

    calculated mean and variance

--- practica2/error_sampling.m
+++ practica2/error_sampling.m
@@ -8,15 +8,19 @@ disp("Loading data...");
 load(datafile);
 disp("Data load complete");
 errorlist = [0.1,0.01,0.001,0.0001,0.00001,.000001,.0000001,.00000001,.000000001,.0000000001,.00000000001,.000000000001,.0000000000001,.00000000000001,.000000000000001,.0000000000000001,.00000000000000001,.000000000000000001,.0000000000000000001,.00000000000000000001];
-errormat = [];
+result = zeros(2,20);
 
 for i = 1:1:20
+  x = [];
   for j = 1:1:30
     [te, tr] = shuffle(data, 0.9, j + i);
     [ps0, ps, ph0, ph] = train_model(tr);
     [ps1, ph1] = laplace_smoother(ps, ph, errorlist(1,i));
-    errormat(j, i) = clasifier_matrix(ps0,ps1,ph0,ph1,te);
+    x(1,j) = full(clasifier_matrix(ps0,ps1,ph0,ph1,te));
   endfor
+  result(1,i) = mean(x);
+  result(2,i) = var(x);
 endfor
 
-save errormat
+save result
+
